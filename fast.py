d35541c2e41a456fc5a0e006607e5291430448e0
Zhou-xjtu-undergraduate <87412545+Zhou-xjtu-undergraduate@users.noreply.github.com>
2022-05-06 13:21:41 +0800

    Update fast.py

--- fast.py
+++ fast.py
@@ -1,4 +1,4 @@
-from math import *
+import math
 from sympy import *
 import numpy as np
 import matplotlib.pyplot as plt
@@ -28,9 +28,9 @@ def minn(f):
 	return t_min
 
 def main(x0,theta):
-	data = []
+	data = list()
 	grand = dif(x0)
-	data1 = [x0,grand]
+	data1 = x0
 	data.append(data1)
 	grand_len = mo(grand)
 	while grand_len > theta:
@@ -39,10 +39,10 @@ def main(x0,theta):
 		t_f = function().subs(x1,x[0]).subs(x2,x[1])
 		t_min = minn(t_f)
 		
-		x0 = x0 +t_min*p
+		x0= x0 +t_min*p
 		grand = dif(x0)
 		grand_len = mo(grand)
-		data1 = [x0,grand]
+		data1 = [x0[0],x0[1]]
 		data.append(data1)	
 	return data	
 def file1(data):
@@ -50,11 +50,10 @@ def file1(data):
 	with open(r"11.text",mode='w',encoding='utf-8') as f:
 		for i in range(0,len(data)):
 			f.write("%d: \t" % (i+1))
-			for j in range(0,2):
-				m = data[i]
-				f.write('f(%.3f)=%.7f\t' % m[j])
+			for j in [0,1]:
+				f.write("%d: \t" % float(data[i][j]))
 			f.write("\n")
 		print("写入成功")	
 		
-data = main([0,0],0.00001)
-print(data)
+data = main([0,0],0.0001)
+file1(data)
